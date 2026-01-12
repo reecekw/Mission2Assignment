32f620aa1a452c1285addb6922c2e8e489896bb7
“reecekw” <reecekw@byu.edu>
2026-01-12 10:16:37 -0700

    Test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Mission2Assignment;
 
-Console.WriteLine("Hello, World!");
+// Welcome the user and ask for the number of rolls
+Console.WriteLine("Welcome to the dice throwing simulator!");
+Console.Write("How many dice rolls would you like to simulate? ");
+
+// Read user input
+if (int.TryParse(Console.ReadLine(), out int numRolls) &&  numRolls >= 1)
+{
+    // Start the second class
+    RollGenerator rg = new RollGenerator();
+    
+    // Pass the number of rolls
+    int[] rollResults = rg.SimulateRolls(numRolls);
+
+    Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+    Console.WriteLine("Each * represents 1% of the total number of rolls.");
+    Console.WriteLine($"Total number of rolls = {numRolls}.\n");
+
+    // Loop through the results for totals 2 through 12
+    for (int i = 2; i <= 12; i++)
+    {
+        // Calculate percentage of total rolls
+        double percentage = ((double)rollResults[i] / numRolls) * 100;
+        
+        // Round to the nearest whole number for the histogram
+        int asteriskCount = (int)Math.Round(percentage);
+
+        Console.Write($"{i}: ");
+        Console.WriteLine(new string('*', asteriskCount));
+    }
+
+    Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+}
+else
+{
+    Console.WriteLine("Please enter a valid number.");
+}
--- RollGenerator.cs
+++ RollGenerator.cs
@@ -1,6 +1,28 @@
-namespace DiceSimulator;
+using System;
 
-public class RollGenerator
+namespace Mission2Assignment
 {
-    
+    public class RollGenerator
+    {
+        public int[] SimulateRolls(int numberOfRolls)
+        {
+            // Array to store counts for the totals
+            int[] results = new int[13];
+            Random random = new Random();
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                // Roll two separate 6-sided dice
+                int die1 = random.Next(1, 7); 
+                int die2 = random.Next(1, 7);
+                int total = die1 + die2;
+
+                // Increment the count for that combination
+                results[total]++;
+            }
+
+            // Return the array back to Program.cs
+            return results;
+        }
+    }
 }
